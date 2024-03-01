05a5eba8ae67ecbc9763ee593041dd8d3b11b57b
ImadUddien9543 <113409331+ImadUddien9543@users.noreply.github.com>
2024-03-02 04:11:12 +0700

    Update PWM_Motor.c

--- BareMetal_TIM/PWM_Motor.c
+++ BareMetal_TIM/PWM_Motor.c
@@ -7,12 +7,15 @@
 
 #include "PWM_Motor.h"
 
-static inline void TIM_CH1(TIM_TypeDef *TIMx, int_fast32_t PWM);
-static inline void TIM_CH2(TIM_TypeDef *TIMx, int_fast32_t PWM);
-static inline void TIM_CH3(TIM_TypeDef *TIMx, int_fast32_t PWM);
-static inline void TIM_CH4(TIM_TypeDef *TIMx, int_fast32_t PWM);
+static inline void TIM_CH1(TIM_TypeDef *TIMx, uint32_t PWM);
+static inline void TIM_CH2(TIM_TypeDef *TIMx, uint32_t PWM);
+static inline void TIM_CH3(TIM_TypeDef *TIMx, uint32_t PWM);
+static inline void TIM_CH4(TIM_TypeDef *TIMx, uint32_t PWM);
 
 static void Rotate_MOTOR(PWM_MOTOR *self, int_fast32_t PWM);
+static void EnableDriver(PWM_MOTOR *self);
+static void DisableDriver(PWM_MOTOR *self);
+static void Interrupt_CB(PWM_MOTOR *self, void(*CB)(uint16_t));
 
 PWM_MOTOR *Init_Motor(TIM_HandleTypeDef *htim_A, TIM_HandleTypeDef *htim_B, uint32_t ch_A, uint32_t ch_B){
 	PWM_MOTOR *self;
@@ -20,27 +23,33 @@ PWM_MOTOR *Init_Motor(TIM_HandleTypeDef *htim_A, TIM_HandleTypeDef *htim_B, uint
 	self->in_A = htim_A->Instance;
 	self->in_B = htim_B->Instance;
 	self->Move_MOTOR = Rotate_MOTOR;
+	self->IntCB = Interrupt_CB;
+
+	self->Enable_MOTOR = EnableDriver;
+	self->Disable_MOTOR = DisableDriver;
+	self->Enable_MOTOR(self);
 
 	(ch_A == TIM_CHANNEL_1) ? (self->Set_CCR_A = TIM_CH1) :
 	(ch_A == TIM_CHANNEL_2) ? (self->Set_CCR_A = TIM_CH2) :
-	(ch_A == TIM_CHANNEL_3) ? (self->Set_CCR_A = TIM_CH3) : (self->Set_CCR_A = TIM_CH4);
+	(ch_A == TIM_CHANNEL_3) ? (self->Set_CCR_A = TIM_CH3) :
+	(ch_A == TIM_CHANNEL_4) ? (self->Set_CCR_A = TIM_CH4) : ((void)0U);
 
 	(ch_B == TIM_CHANNEL_1) ? (self->Set_CCR_B = TIM_CH1) :
 	(ch_B == TIM_CHANNEL_2) ? (self->Set_CCR_B = TIM_CH2) :
-	(ch_B == TIM_CHANNEL_3) ? (self->Set_CCR_B = TIM_CH3) : (self->Set_CCR_B = TIM_CH4);
+	(ch_B == TIM_CHANNEL_3) ? (self->Set_CCR_B = TIM_CH3) :
+	(ch_B == TIM_CHANNEL_4) ? (self->Set_CCR_B = TIM_CH4) : ((void)0U);
 
 	return self;
 }
 
 static void Rotate_MOTOR(PWM_MOTOR *self, int_fast32_t PWM){
-	uint32_t PWM_ = abs(PWM);
-	if(PWM < 0){
-		self->Set_CCR_A(self->in_A, PWM_);
+	if(PWM > 0){
+		self->Set_CCR_A(self->in_A, (uint32_t)PWM);
 		self->Set_CCR_B(self->in_B, 0);
 	}
-	else if(PWM > 0){
+	else if(PWM < 0){
 		self->Set_CCR_A(self->in_A, 0);
-		self->Set_CCR_B(self->in_B, PWM_);
+		self->Set_CCR_B(self->in_B, (uint32_t)abs(PWM));
 	}
 	else{
 		self->Set_CCR_A(self->in_A, 0);
@@ -48,15 +57,27 @@ static void Rotate_MOTOR(PWM_MOTOR *self, int_fast32_t PWM){
 	}
 }
 
-static inline void TIM_CH1(TIM_TypeDef *TIMx, int_fast32_t PWM){
+static inline void TIM_CH1(TIM_TypeDef *TIMx, uint32_t PWM){
 	TIMx->CCR1 = PWM;
 }
-static inline void TIM_CH2(TIM_TypeDef *TIMx, int_fast32_t PWM){
+static inline void TIM_CH2(TIM_TypeDef *TIMx, uint32_t PWM){
 	TIMx->CCR2 = PWM;
 }
-static inline void TIM_CH3(TIM_TypeDef *TIMx, int_fast32_t PWM){
+static inline void TIM_CH3(TIM_TypeDef *TIMx, uint32_t PWM){
 	TIMx->CCR3 = PWM;
 }
-static inline void TIM_CH4(TIM_TypeDef *TIMx, int_fast32_t PWM){
+static inline void TIM_CH4(TIM_TypeDef *TIMx, uint32_t PWM){
 	TIMx->CCR4 = PWM;
+}
+
+static void Interrupt_CB(PWM_MOTOR *self, void(*CB)(uint16_t)){
+	CB = HAL_GPIO_EXTI_Callback;
+	CB(self->ch_A);
+}
+
+static void EnableDriver(PWM_MOTOR *self){
+	HAL_GPIO_WritePin(self->en_port, self->en_pin, GPIO_PIN_SET);
+}
+static void DisableDriver(PWM_MOTOR *self){
+	HAL_GPIO_WritePin(self->en_port, self->en_pin, GPIO_PIN_RESET);
 }
